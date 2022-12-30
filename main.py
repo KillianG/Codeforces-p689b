e013a284b888f46a22e9a1dd247cec445531d77b
Killian Gardahaut <killian.gardahaut@gmail.com>
2022-12-30 16:07:51 +0000

    better

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-if __name__ == '__main__':
-    nb_intersec = int(input())
-    shortcuts = [int(item) for item in input().split(' ')]
+def distance(start, arrival):
+    return abs(start - arrival)
+
+def mike_shortcut(nb_intersec, shortcuts):
     for i in range(1, nb_intersec+1):
         nrj = 0
         s = 1
-        while s <= len(shortcuts):
-            if s >= i:
-                print(nrj, end=' ')
-                break
+        while s != i :
             nrj+=1
-            if shortcuts[s-1] <= i and shortcuts[s-1] > s:
+            if distance(s, i) > distance(shortcuts[s-1], i):
                 s = shortcuts[s]
                 continue
-            s+=1
+            if (s > i):
+              s -= 1
+            else:
+              s += 1
+        print(nrj, end=' ')
     print()
+
+
+if __name__ == '__main__':
+    nb_intersec = int(input())
+    shortcuts = [int(item) for item in input().split(' ')]
+    mike_shortcut(nb_intersec, shortcuts)

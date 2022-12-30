7bf0714c1d14524ee264774f88d2b5126d6f3336
Killian Gardahaut <killian.gardahaut@gmail.com>
2022-12-30 15:13:36 +0000

    q

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 if __name__ == '__main__':
     nb_intersec = int(input())
     shortcuts = [int(item) for item in input().split(' ')]
-    print(list(shortcuts))
+    for i in range(nb_intersec+1):
+        for s in range(len(shortcuts)):
+            if s == i:
+                print(s, end=' ')
+                break
+    print()
